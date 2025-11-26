c7eb98e6636f1e0daaf46bd59ba10e071d6e33b2
“euon05 <yslove1670735@naver.com>
2025-11-26 15:43:57 +0900

    업데이트 내용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import { NextAuthProvider } from '@/components/Providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -18,20 +19,24 @@ export const metadata: Metadata = {
   description: 'Create, Read, Update, and Delete in MongoDB',
 }
 
+// 1. props에 타입 정의 추가 ({ children: React.ReactNode })
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
+      {/* 2. 존재하지 않는 inter 대신 geistSans와 geistMono 변수 사용 */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="max-w-4xl mx-auto">
-          <Navbar />
-          <div className="mt-8">{children}</div>
-        </div>
+        <NextAuthProvider>
+          <div className="max-w-3xl mx-auto p-4">
+            <Navbar />
+            <div className="mt-8">{children}</div>
+          </div>
+        </NextAuthProvider>
       </body>
     </html>
   )
